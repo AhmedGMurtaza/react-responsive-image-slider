0cbb2a613bb4274faef58f99d834412c53373c1a
Ahmed Murtaza <beyondmy_destiny@hotmail.com>
2018-05-18 02:53:36 -0400

    update directory

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Slider from './Slider';
-import Slide from './Slide';
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Slider from "./scripts/Slider";
+import Slide from "./scripts/Slide";
 
 ReactDOM.render(
-    <Slider>
-        <Slide cover="../img/1.jpg">
-            {/* Add your desired content inside */}
-        </Slide>
-        <Slide cover="../img/2.jpg">
-            <div className="slide-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. </div>
-        </Slide>
-        <Slide cover="../img/3.jpg">
-            <div className="slide-text">1</div>
-        </Slide>
-        <Slide cover="../img/4.jpg">4</Slide>
-        <Slide cover="../img/5.jpg">5</Slide>
-        <Slide cover="../img/6.jpg">6</Slide>
-    </Slider>
-    , document.getElementById('root'));
+  <Slider>
+    <Slide cover="../img/1.jpg">{/* Add your desired content inside */}</Slide>
+    <Slide cover="../img/2.jpg">
+      <div className="slide-text">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit.{" "}
+      </div>
+    </Slide>
+    <Slide cover="../img/3.jpg">
+      <div className="slide-text">1</div>
+    </Slide>
+    <Slide cover="../img/4.jpg">4</Slide>
+    <Slide cover="../img/5.jpg">5</Slide>
+    <Slide cover="../img/6.jpg">6</Slide>
+  </Slider>,
+  document.getElementById("root")
+);
